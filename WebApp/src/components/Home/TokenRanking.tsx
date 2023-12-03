7d21b0d929e23c35a4a2ce5db5164d9d425ab25b
lorena40m <junjunhere3@gmail.com>
2023-12-03 19:56:42 +0100

    slider for coins on homepage

--- WebApp/src/components/Home/TokenRanking.tsx
+++ WebApp/src/components/Home/TokenRanking.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from "next-i18next"
 import { useRouter } from "next/router"
 import { formatPrice } from '../../utils/format'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay, Pagination, Navigation } from "swiper"
+import { Autoplay, Mousewheel, Navigation } from "swiper"
 // Import Swiper styles
 import "swiper/css"
 import "swiper/css/pagination"
@@ -17,51 +17,49 @@ export default function TokenRanking({ coins, rates }) {
   const { locale } = useRouter()
 
   return (
-    <>
-    {/*<Swiper
-      className="TokenRanking-Swiper"
-      direction={'vertical'}
-      height={300}
-      slidesPerView={3}
-      spaceBetween={15}
-      grid={{ rows: 2 }}
-      modules={[Autoplay, Navigation]}
-      loop={false}
-      autoplay={{
-        delay: 1000,
-        disableOnInteraction: false,
-      }}
-      navigation={false}
-    >
-      <SwiperSlide className="TokenRanking-slide">Slide 1</SwiperSlide>
-      <SwiperSlide className="TokenRanking-slide">Slide 2</SwiperSlide>
-      <SwiperSlide className="TokenRanking-slide">Slide 3</SwiperSlide>
-      <SwiperSlide className="TokenRanking-slide">Slide 4</SwiperSlide>
-      <SwiperSlide className="TokenRanking-slide">Slide 5</SwiperSlide>
-      <SwiperSlide className="TokenRanking-slide">Slide 6</SwiperSlide>
-    </Swiper>*/}
     <Box className="TokenRanking">
-      <div className="TokenRanking-container">
-        {coins.map((coin, i) => (
-          <Link key={coin.id + coin.token_id} href={'/' + coin.id}
-            className={"TokenRanking-TokenBox"}
-          >
-            <div className="TokenRanking-line">
-              <div className="TokenRanking-col">
-                <img src={coin.logo} alt={coin.ticker} width={64} />
-              </div>
-              <div className="TokenRanking-col">
-                <div className="TokenRanking-names" title={coin.name}>
-                  <span className="TokenRanking-name" title={coin.name}>{coin.name}</span>
-                  <span className="TokenRanking-ticker">{coin.ticker}</span>
+      <div className="TokenRanking-container" style={{height: "360px"}}>
+        <Swiper
+          direction="vertical"
+          height={120}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Navigation, Mousewheel]}
+          mousewheel={true}
+          loopedSlides={3}
+        >
+          {coins.map((coin, i) => (
+            i % 4 === 0 && (
+              <SwiperSlide key={i}>
+                <div className="TokenRanking-group">
+                  {coins.slice(i, i + 4).map((coin, j) => (
+                    <Link key={coin.id + coin.token_id} href={'/' + coin.id}
+                    className={"TokenRanking-TokenBox"}
+                    >
+                      <div className="TokenRanking-line">
+                        <div className="TokenRanking-col">
+                          <img src={coin.logo} alt={coin.ticker} width={64} />
+                        </div>
+                        <div className="TokenRanking-col">
+                          <div className="TokenRanking-names" title={coin.name}>
+                            <span className="TokenRanking-name" title={coin.name}>{coin.name}</span>
+                            <span className="TokenRanking-ticker">{coin.ticker}</span>
+                          </div>
+                          <span className="TokenRanking-price">{formatPrice(coin.lastPrice, locale, rates)}</span>
+                        </div>
+                      </div>
+                    </Link>
+                  ))}
                 </div>
-                <span className="TokenRanking-price">{formatPrice(coin.lastPrice, locale, rates)}</span>
-              </div>
-            </div>
-          </Link>
-        ))}
+              </SwiperSlide>
+            )
+          ))}
+        </Swiper>
       </div>
     </Box>
-    </>
   )
 }
