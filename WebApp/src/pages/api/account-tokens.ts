b2fa5e31a6cc16f598f370a36edebd56f3cef2f4
lorena40m <junjunhere3@gmail.com>
2023-12-22 17:35:41 +0100

    wip : select only coins holded by account

--- WebApp/src/pages/api/account-tokens.ts
+++ WebApp/src/pages/api/account-tokens.ts
@@ -8,7 +8,28 @@ export default async function handler(
   const address = req.query.address;
 
   try {
-    const { rows: tokens } = await pool.query('select "TokenBalances"."Balance", "TokenBalances"."TokenId", "Tokens"."ContractId", "Tokens"."Metadata", a4."Address" from "Accounts" INNER join "TokenBalances" ON "Accounts"."Id" = "TokenBalances"."AccountId" INNER join "Tokens" ON "TokenBalances"."TokenId" = "Tokens"."Id" LEFT JOIN "Accounts" as a4 ON ("Tokens"."ContractId" = a4."Id") where "Accounts"."Address" = $1 and "TokenBalances"."Balance" <> \'0\'', [address]);
+    const { rows: tokens } = await pool.query('
+
+    SELECT
+        "TokenBalances"."TokenId",
+        SUM("TokenBalances"."Balance"::NUMERIC) AS "TotalBalance",
+        "Tokens"."ContractId",
+        "Tokens"."Metadata"
+    FROM
+        "Accounts"
+    INNER JOIN
+        "TokenBalances" ON "Accounts"."Id" = "TokenBalances"."AccountId"
+    INNER JOIN
+        "Tokens" ON "TokenBalances"."TokenId" = "Tokens"."Id"
+    WHERE
+        "Accounts"."Address" = $1 AND "TokenBalances"."Balance" <> '0'
+    GROUP BY
+        "TokenBalances"."TokenId",
+        "Tokens"."ContractId",
+        "Tokens"."Metadata"
+    HAVING
+        MAX(("Tokens"."Metadata"->>'decimals')::INT) > 0;
+      ', [address]);
     // recuperer le storage des tokens afin de calculer le prix des tokens via : token_pool / tez_pool
     // sinon utiliser : https://temple-api-mainnet.prod.templewallet.com/api/exchange-rates
     res.status(200).json(tokens);
