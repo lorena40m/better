be637ce6d1b325e6433e75452333b57b2559c0f4
lorena40m <junjunhere3@gmail.com>
2024-01-21 12:02:57 +0100

    BACK: add logMode for db query

--- WebApp/src/endpoints/providers/db.ts
+++ WebApp/src/endpoints/providers/db.ts
@@ -4,14 +4,15 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-export async function query(name, sql, params): Promise<any[]> {
+export async function query(name, sql, params, logMode: 'short' | 'long' = null): Promise<any[]> {
   try {
     const a = performance.now()
     const rows = (await pool.query(sql, params)).rows
     const b = performance.now()
     if (process.env.NODE_ENV === 'development')
-      console.info(`Query ${name} executed in ${(b - a)/1000}s, ${rows.length} rows fetched.`)
-    console.log(rows)
+      console.info(`Query ${name}[${params}] executed in ${(b - a)/1000}s, ${rows.length} rows fetched.`)
+    if ((logMode ?? process.env.DB_LOG_MODE) === 'long')
+      console.log(rows)
     return rows
   } catch (error) {
     throw `Error on query ${name}: ${error}`
